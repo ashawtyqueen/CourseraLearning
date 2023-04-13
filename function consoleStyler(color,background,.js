e57f2f56bfbd52b1e3c4374dfce6c799b7454e07
Tokede Anuoluwapo <tokede29@gmail.com>
2023-04-13 11:19:38 +0100

    NEW

---
+++ function consoleStyler(color,background,.js
@@ -0,0 +1,8 @@
+function consoleStyler(color,background,fontSize,txt) {
+    var message = "%c" + txt;
+    var style = `color: ${color};`
+    style += `background: ${background};'
+    style += `font-size: ${ fontSize };`
+    
+    console.log(consoleStyler);
+}
